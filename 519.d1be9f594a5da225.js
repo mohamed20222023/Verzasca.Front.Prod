2331dd999d5ec7adb47ff019baeffc4fa6dbce37
Mohamed Eid <71554599+mohamed20222023@users.noreply.github.com>
2025-02-17 11:50:42 +0200

    publish20250217_0

---
+++ 519.d1be9f594a5da225.js
@@ -0,0 +1,1 @@
+"use strict";(self.webpackChunkvelzon=self.webpackChunkvelzon||[]).push([[519],{66900:(y,p,c)=>{c.d(p,{JG:()=>m});var D=c(54438);let m=(()=>{class t{static \u0275fac=function(r){return new(r||t)};static \u0275mod=D.$C({type:t});static \u0275inj=D.G2t({})}return t})()}}]);
